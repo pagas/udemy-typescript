17f89491f0816ea6f65cba544869c7b4c9224287
Paulius G <paulius@2simple.com>
2023-09-23 18:04:44 +0100

    add class-validator library and use it

--- third-party-libs/src/app.ts
+++ third-party-libs/src/app.ts
@@ -2,11 +2,22 @@ import _ from 'lodash';
 import {Product} from './product.model';
 import 'reflect-metadata';
 import {plainToInstance} from "class-transformer";
+import {validate} from "class-validator"
 
 const products = [
     {title: 'A carpet', price: 29.99},
     {title: 'A book', price: 10.99}
 ]
+const newProd = new Product('Empty book', -5);
+
+validate(newProd).then((errors) => {
+    if (errors.length > 0) {
+        console.log("Errors: ", errors);
+    } else {
+        console.log(newProd.getInformation());
+    }
+});
+
 
 const product = new Product('A book', 12.99)
 const loadedProducts = plainToInstance(Product, products)
