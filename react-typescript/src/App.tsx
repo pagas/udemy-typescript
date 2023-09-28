35a21db89d0f5e8e72c3de7b3c863911a2767124
Paulius G <paulius@2simple.com>
2023-09-28 23:41:40 +0100

    add ability to delete todo

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -6,13 +6,16 @@ import { Todo } from './todo.model';
 const App = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
   const todoAddHandler = (text: string) => {
-    setTodos([...todos, { id: Math.random().toString(), text: text }])
+    setTodos(prevState => [...prevState, { id: Math.random().toString(), text: text }])
+  }
+  const deleteHandler = (id: string) => {
+    setTodos(prevState => prevState.filter(todo => todo.id !== id))
   }
   return (
     <div className="App">
       <h1>Todo</h1>
       <NewTodo onAddTodo={todoAddHandler} />
-      <TodoList todos={todos} />
+      <TodoList todos={todos} onDelete={deleteHandler} />
     </div>
   );
 }
--- react-typescript/src/components/TodoList.tsx
+++ react-typescript/src/components/TodoList.tsx
@@ -2,13 +2,23 @@ import React from 'react';
 import { Todo } from '../todo.model';
 
 type TodoProps = {
-    todos: Todo[]
+    todos: Todo[],
+    onDelete: (id: string) => void
 }
 
-const TodoList = ({ todos }: TodoProps) => {
+const TodoList = ({ todos, onDelete }: TodoProps) => {
+
+    const deleteHandler = (id: string) => {
+        onDelete(id);
+    }
 
     return <ul>
-        {todos.map(todo => <li key={todo.id}>{todo.text}</li>)}
+        {todos.map(todo => (
+            <li key={todo.id}>
+                <span>{todo.text}</span>
+                <button onClick={() => deleteHandler(todo.id)}>Delete</button>
+            </li>
+        ))}
     </ul>
 }
 
